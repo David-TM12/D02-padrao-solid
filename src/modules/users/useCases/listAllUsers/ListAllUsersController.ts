0bfe0e8773ec0e05c7ecfbf5b16eacd4c401c8a0
DavidTM <deivi_gf144@hotmail.com>
2021-07-28 21:33:44 -0400

    implementando funcionalidade

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,29 @@
 import { Request, Response } from "express";
-
+import { User } from "modules/users/model/User";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+interface IRequest {
+  user_id: string;
+}
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+
+    try{
+
+      const { user_id } = request.headers;
+
+      const id = user_id.toString();
+
+      const users = this.listAllUsersUseCase.execute({ user_id: id });
+
+      return response.status(200).json(users);
+
+    }catch(e){
+      return response.status(400).json({error: e.message});
+    }
+   
   }
 }
 
